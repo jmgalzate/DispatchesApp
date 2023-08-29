1465c70216c9e5ce8fb4d19c1cd5593a96acc5e1
Juan Miguel <jmgalzate@outlook.com>
2023-08-28 23:26:55 -0500

    Handling with Visual effect the Session Status

--- FioryApp/src/Service/SessionService.cs
+++ FioryApp/src/Service/SessionService.cs
@@ -1,9 +1,19 @@
-﻿namespace FioryApp.src.Service
+﻿using FioryLibrary.Connections;
+using Newtonsoft.Json.Linq;
+
+namespace FioryApp.src.Service
 {
 	public class SessionService
 	{
         public string KeyAgent { get; set; } = ""; // Initialize as empty string
 
-	}
+        public event EventHandler SessionStateChanged;
+
+        public void UpdateSessionKeyAgent(string keyAgent)
+        {
+            KeyAgent = keyAgent;
+            SessionStateChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
 
